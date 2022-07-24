f36b75067fe04a66e6ad65ef8f814d84e0733f49
CarLoWorld <ben.carlo.cornico@gmail.com>
2022-07-24 14:18:02 -0700

    loading product function

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
-import './components.css'
+import './components.css';
+import productData from './prod/ProductData';
 
 export class Shop extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      products: productData,
+    }
+    console.log(productData)
+
+  }
   render() {
     return (
       <div className='shop-div'>
         <div className="shop-list"  onClick={this.props.addCart}>
-
-          shoip list
+          {
+            this.state.products.kids.map(prod => {
+              return (
+                <div>{prod.name}
+                  {
+                    prod.img.map(prodImg => {
+                      
+                      return <img src={prodImg} alt="" srcset="" />
+                    })
+                  }
+                </div>
+              )
+            })
+          }
         </div>
       </div>
     )
