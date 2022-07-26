d9979b8cd5b89ae1228fdee882554d320f2b5cc6
CarLoWorld <ben.carlo.cornico@gmail.com>
2022-07-25 22:07:46 -0700

    Home page design update

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,21 @@ import React, { Component } from 'react'
 export class Home extends Component {
   render() {
     return (
-      <div>Home</div>
+        <div className="home">
+          <div className="home-content">
+            <p className="main-line">
+              Walk with no worry.
+            </p>
+            <p className="offer-line">
+              100% water proof shoes for any season.
+            </p>
+            <div className="btn-group-home">
+              <button className='btn-home'>Shop Women</button>
+              <button className='btn-home'>Shop Men</button>
+            </div>
+          </div>
+
+        </div>
     )
   }
 }
